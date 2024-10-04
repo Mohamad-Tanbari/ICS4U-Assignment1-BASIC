5faccecb64abe933dd0c20a5a656acc5d83a57fa
Mohamad Tanbari <mohamad.tanbari@mths.ca>
2024-10-04 14:03:41 +0000

    started TS version of game

---
+++ main.ts
@@ -0,0 +1,21 @@
+/*
+ * This program is a russian roulette game.
+ *
+ * By: Mohamad T
+ * Version: 1.0
+ * Since: 2024-10-4
+ */
+
+import { createPrompt } from 'bun-promptx'
+
+// Start
+console.log('THIS IS A GAME OF >>>>>>>>>>>>>>>RUSSIAN ROULETTE')
+console.log('')
+console.log('HERE IS A REVOLVER')
+console.log('       (HIT `2` TO GIVE UP)')
+console.log('GO')
+
+let turns = 0
+
+
+
