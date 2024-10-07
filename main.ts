215358f573b354bd8976c4237392d39e1e9bdd78
Mohamad Tanbari <mohamad.tanbari@mths.ca>
2024-10-07 13:53:39 +0000

    RR in typescript

--- main.ts
+++ main.ts
@@ -13,33 +13,62 @@ console.log('THIS IS A GAME OF >>>>>>>>>>>>>>>RUSSIAN ROULETTE')
 console.log('')
 
 let turns = 0
-let tempCounter = 0
-while (tempCounter < 6) {
-  console.log('HERE IS A REVOLVER')
+let nextGame = false
+let exit = false
+
+while (true) {
+  if (turns === 10) {
+    console.log('You Win!')
+    break
+  }
+  console.log(`${nextGame ? '...NEXT VICTIM...' : 'HERE IS A REVOLVER'}`)
   console.log('HIT `1` TO SPIN CHAMBER AND PULL TRIGGER')
   console.log('       (HIT `2` TO GIVE UP)')
   console.log('GO')
 
-  const input = createPrompt('? ')
+  while (turns < 6) {
+    const input = createPrompt('? ')
 
-  if (isNaN(input.value)) {
-    console.log('Invalid input. Try again')
-  } else {
-    if (input.value !== 2) {
-      turns++
-
-      // Generate random number
-      const randomNumber = Math.random()
-      
-      if (randomNumber > 0.8333) {
-        console.log('      BANG!!! YOU\'RE DEAD!')
-        console.log()
-      }
+    if (isNaN(input.value)) {
+      console.log('Invalid input. Try again')
     } else {
-      console.log('      CHICKEN!!!')
+      if (parseInt(input.value) !== 2) {
+        turns++
+
+        // Generate random number
+        const randomNumber = Math.random()
+
+        if (randomNumber > 0.8333) {
+          console.log('      BANG!!! YOU\'RE DEAD!')
+          console.log('CONDOLENCES WILL BE SENT TO YOUR RELATIVES.')
+          nextGame = true
+          turns = 0
+          break
+        } else {
+          console.log('- CLICK -')
+        }
+      } else {
+        console.log('      CHICKEN!!!')
+        exit = true
+        break
+      }
+    }
+    console.log('\n\n')
+
+    // Check if the user won
+    if (turns === 6) {
+      console.log('YOU WIN !!!')
+      console.log('LET SOMEONE ELSE BLOW HIS BRAINS OUT.')
+      turns = 0
+      break
     }
   }
+  console.log('\n\n')
 
-  tempCounter++
+  if (exit) {
+    break
+  }
 }
 
+// Done
+console.log('\nDone.')
